021b0e60ecdef4313cf506887624bb93892ac560
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-06-12 11:19:11 -0700

    use poisson distribution to calculate MOI

--- muxsim/gen.py
+++ muxsim/gen.py
@@ -8,67 +8,36 @@ class MuxSim:
         num_guides: int = 100,
         n: float = 10.0,
         p: float = 0.1,
-        null_rate: float = 0.3,
-        dual_rate: float = 0.2,
+        λ: float = 0.8,
         random_state: int = 42,
     ):
         self.num_cells = num_cells
         self.num_guides = num_guides
         self.n = n
         self.p = p
-        self.null_rate = null_rate
-        self.dual_rate = dual_rate
+        self.λ = λ
         self.random_state = random_state
 
         np.random.seed(self.random_state)
         self.umi_sums = self._gen_umi_sums()
-        self.null_assignments = self._gen_null_assignments()
-        self.assignments = self._gen_assignments(self.null_assignments)
-        self.dual_assignments = self._gen_dual_assignments(self.assignments)
-
-        self.mask_null = self.null_assignments == 1
-        self.mask_dual = self.dual_assignments != -1
-        self.mask_assignment = self.assignments != -1
-        self.mask_single = self.mask_assignment & ~self.mask_dual
-
+        self.moi = self._gen_moi()
+        self.assignments = self._gen_assignments()
         self.mean_umi = self.umi_sums.mean()
 
     def _gen_umi_sums(self) -> np.ndarray:
         """Generate UMI sums for each cell/guide pair."""
         return np.random.negative_binomial(self.n, self.p, size=self.num_cells)
 
-    def _gen_null_assignments(self) -> np.ndarray:
-        return np.random.binomial(1, self.null_rate, size=self.num_cells)
+    def _gen_moi(self) -> np.ndarray:
+        """Generate MOI for each cell/guide pair."""
+        return np.random.poisson(self.λ, size=self.num_cells)
 
-    def _gen_assignments(self, null: np.ndarray) -> np.ndarray:
+    def _gen_assignments(self) -> list:
         """Generate assignments for each cell/guide pair."""
-        assignment = np.random.choice(
-            np.arange(self.num_guides), size=self.num_cells, replace=True
-        )
-        assignment[null == 1] = -1
+        assignment = [
+            np.random.choice(self.num_guides, self.moi[i]) for i in range(self.num_cells)
+        ]
         return assignment
-
-    def _gen_dual_assignments(self, assignments: np.ndarray) -> np.ndarray:
-        """Generate dual assignments for each cell/guide pair."""
-        single_idx = np.flatnonzero(assignments != -1)
-        num_single = single_idx.size
-
-        # Only generate dual assignments for cells with single assignments
-        mask = np.random.binomial(1, self.dual_rate, size=num_single)
-        single_mask = single_idx[mask != 1]
-
-        # Generate dual assignments
-        secondary = np.random.choice(
-            np.arange(self.num_guides), size=self.num_cells, replace=True
-        )
-
-        # Set all null assignments to -1
-        secondary[assignments == -1] = -1
-
-        # Set all dual assignments that are not singles to -1
-        secondary[single_mask] = -1
-
-        return secondary
 
     def __repr__(self):
         return f"""MuxSim(
@@ -76,22 +45,19 @@ class MuxSim:
     num_guides={self.num_guides},
     n={self.n},
     p={self.p},
-    null_rate={self.null_rate},
-    dual_rate={self.dual_rate},
+    λ={self.λ},
     random_state={self.random_state}
     mean_umi={self.mean_umi})"""
 
-    def sample(self, signal: float = 10.0, background: float = 0.01):
+    def sample(self, signal: float = 10.0):
         """Sample from Multinomial distribution."""
+        background = 1.0 / self.num_guides
         freq = np.repeat(background, self.num_guides)
-        freq /= freq.sum()
         umi = np.zeros((self.num_cells, self.num_guides), dtype=int)
         for i in range(self.num_cells):
             subfreq = freq.copy()
-            if self.assignments[i] != -1:
+            if self.moi[i] != 0:
                 subfreq[self.assignments[i]] = signal * background
-                if self.dual_assignments[i] != -1:
-                    subfreq[self.dual_assignments[i]] = signal * background
                 subfreq /= subfreq.sum()
             umi[i] = np.random.multinomial(self.umi_sums[i], subfreq)
         return umi
